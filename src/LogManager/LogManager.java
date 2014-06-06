57ce6377096c04f224b3a1d47b9db3f9823c6429
AmirGeorge <dangerousmiro@gmail.com>
2014-06-07 02:17:45 +0300

    LogM awaiting PageID, recM class structure

--- src/LogManager/LogManager.java
+++ src/LogManager/LogManager.java
@@ -1,23 +1,97 @@
 package LogManager;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Hashtable;
 
-public interface LogManager {
-	public void init();
+public class LogManager implements LogManagerInterface {
+	BufferedReader br;
+	FileWriter fw;
+	String path;
+	String dataToBeFlushed = "";
 
-	public void flushLog();
+	public static void main(String[] args) throws IOException {
 
-	public void recordStart(String strTransID);
+		LogManager x = new LogManager();
+		x.init();
+		x.recordStart("miro");
+		Hashtable<String, String> old = new Hashtable<String,String>();
+		old.put("rab3a","ramz el somood");
+		Hashtable<String, String> new1 = new Hashtable<String,String>();
+		new1.put("rabia","fel qalb");
+		x.recordUpdate("miro", "25-4968", "Name", old, new1);
+		x.recordInsert("miro", old);
+		x.recordDelete("miro", "25-4968", new1);
+		x.recordCommit("miro");
+		x.flushLog();
 
-	public void recordUpdate(String strTransID, /* PageID page, */
-			String strKeyValue, String strColName, Object objOld, Object objNew);
+	}
 
-	public void recordInsert(String strTransID,
-	/* PageID page, */
-	Hashtable<String, String> htblColValues);
+	@Override
+	public void init() {
+		try {
+			br = new BufferedReader(new FileReader(new File(
+					"config/DBApp.properties")));
+			String line;
+			while ((!((line = br.readLine()).split("=")[0] == null))
+					&& (!line.split("=")[0].equals("LogfilePath")))
+				;
+			path = line.split("=")[1];
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-	public void recordDelete(String strTransID, /*PageID page,*/
-			String strKeyValue, Hashtable<String, String> htblColValues);
+	@Override
+	public synchronized void flushLog() {
+		try {
+			fw = new FileWriter(path, true);
+			fw.append(dataToBeFlushed);
+			fw.close();
+			dataToBeFlushed = "";
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-	public void recordCommit(String strTransID);
+	}
+
+	@Override
+	public synchronized void recordStart(String strTransID) {
+		dataToBeFlushed += "start," + strTransID + "\n";
+	}
+
+	@Override
+	public synchronized void recordUpdate(String strTransID,
+			String strKeyValue, String strColName, Object objOld, Object objNew) {
+		dataToBeFlushed += "update," + strTransID + "," + strKeyValue + ","
+				+ strColName + "," + objOld.toString() + ","
+				+ objNew.toString() + "\n";
+
+	}
+
+	@Override
+	public synchronized void recordInsert(String strTransID,
+			Hashtable<String, String> htblColValues) {
+		dataToBeFlushed += "insert," + strTransID + ","
+				+ htblColValues.toString() + "\n";
+
+	}
+
+	@Override
+	public synchronized void recordDelete(String strTransID,
+			String strKeyValue, Hashtable<String, String> htblColValues) {
+		dataToBeFlushed += "delete," + strTransID + "," + strKeyValue + ","
+				+ htblColValues.toString() + "\n";
+
+	}
+
+	@Override
+	public synchronized void recordCommit(String strTransID) {
+		dataToBeFlushed += "commit," + strTransID + "\n";
+	}
+
 }
