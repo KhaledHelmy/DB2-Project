c1e59afbd6491477de4973d3c9d20d20769dd209
OmarElAzazy <oa12gb@gmail.com>
2014-06-08 01:45:43 +0300

    Added TransactionManager and updated DBApp

--- src/Transactions/Transaction.java
+++ src/Transactions/Transaction.java
@@ -1,4 +1,4 @@
-package Utilities;
+package Transactions;
 
 import java.util.Iterator;
 import java.util.Vector;
@@ -8,71 +8,74 @@ import LogManager.LogManager;
 import Steps.Step;
 
 public class Transaction {
-	
+
 	private BufferManager bufferManager;
 	private LogManager logManager;
 	private Vector<Step> steps;
 	private String ID;
-	
+
 	public String getID() {
 		return ID;
 	}
-	
+
 	public void setID(String ID) {
 		this.ID = ID;
 	}
-	
+
 	public BufferManager getBufferManager() {
 		return bufferManager;
 	}
-	
+
 	public void setBufferManager(BufferManager bufferManager) {
 		this.bufferManager = bufferManager;
 	}
-	
+
 	public LogManager getLogManager() {
 		return logManager;
 	}
-	
+
 	public void setLogManager(LogManager logManager) {
 		this.logManager = logManager;
 	}
-	
+
 	public Vector<Step> getSteps() {
 		return steps;
 	}
-	
+
 	public void setSteps(Vector<Step> steps) {
 		this.steps = steps;
 	}
-	
-	public Transaction(){
+
+	public Transaction() {
 		setID(System.currentTimeMillis() + "" + Math.random());
 	}
-	
-	public void init(BufferManager bufManager,
-			LogManager logManager,
-			Vector<Step> vSteps){
+
+	public void init(BufferManager bufManager, LogManager logManager,
+			Vector<Step> vSteps) {
 		setBufferManager(bufManager);
 		setLogManager(logManager);
 		setSteps(vSteps);
 	}
-	
-	public void kill(){
+
+	public void kill() {
 		setBufferManager(null);
 		setLogManager(null);
 		setSteps(null);
 	}
-	
-	public void execute(){
-		getLogManager().recordStart(getID());
-		for(Iterator<Step> it = getSteps().iterator(); it.hasNext();){
-			Step step = it.next();
-			step.execute(this);
-		}
-		
-		getLogManager().recordCommit(getID());
-		getLogManager().flushLog();
-		kill();
+
+	public void execute() {
+		new Thread() {
+			public void run() {
+				getLogManager().recordStart(getID());
+				for (Iterator<Step> it = getSteps().iterator(); it.hasNext();) {
+					Step step = it.next();
+					// TODO step.execute(this);
+				}
+
+				getLogManager().recordCommit(getID());
+				getLogManager().flushLog();
+				kill();
+			}
+		}.start();
 	}
 }
