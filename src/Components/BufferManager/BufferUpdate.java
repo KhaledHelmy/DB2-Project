b62a53677a1b21842e8c8378dfa6fe01b24e3036
Khaled Helmy <khaled.a.helmy@gmail.com>
2014-06-08 01:01:49 +0200

    Added thread to BufferManager

---
+++ src/Components/BufferManager/BufferUpdate.java
@@ -0,0 +1,16 @@
+package Components.BufferManager;
+
+public class BufferUpdate extends Thread {
+	public void run() {
+		try {
+			while (true) {
+				long millis = 120000;
+				Thread.sleep(millis);
+				BufferManager bufferManager = BufferManager.getBufferManager();
+				bufferManager.runLRU();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
