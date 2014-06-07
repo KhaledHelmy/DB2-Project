df0fb1f8b0b93568c305c08ea4fa33680d57e5b7
Khaled Helmy <khaled.a.helmy@gmail.com>
2014-06-07 08:04:48 +0200

    Added singleton design pattern for BufferManager

--- src/Components/BufferManager/BufferManager.java
+++ src/Components/BufferManager/BufferManager.java
@@ -1,5 +1,22 @@
 package Components.BufferManager;
 
-public class BufferManager {
+import java.util.PriorityQueue;
 
+import Utilities.Page;
+
+public class BufferManager {
+	private static BufferManager bufferManager = new BufferManager();  
+	
+	private static final int SIZE = 1000;
+	private int minEmptySlotSize, maxUsedSlotSize;
+	private PriorityQueue<Integer> emptySlots, usedSlots;
+	private Page[] memory;
+	
+	private BufferManager() {
+		
+	}
+	
+	public static BufferManager getInstance() {
+		return bufferManager;
+	}
 }
