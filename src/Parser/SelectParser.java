c9b363ec48a3ca9f4251ff4785743fe2fe561708
MussabElDash <mussab14@gmail.com>
2014-06-08 02:09:13 +0300

    Add select parser

--- src/Parser/SelectParser.java
+++ src/Parser/SelectParser.java
@@ -1,9 +1,19 @@
 package Parser;
 
+import gudusoft.gsqlparser.nodes.TExpression;
 import gudusoft.gsqlparser.stmt.TSelectSqlStatement;
-import Interfaces.StatementParserInterface;
 
-public class SelectParser implements StatementParserInterface {
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import Abtracts.ConditionalParsers;
+import Exceptions.DBEngineException;
+import Utilities.Record;
+import Utilities.Table;
+
+public class SelectParser extends ConditionalParsers {
 	private TSelectSqlStatement stmnt;
 
 	public SelectParser(TSelectSqlStatement stmnt) {
@@ -11,7 +21,34 @@ public class SelectParser implements StatementParserInterface {
 	}
 
 	@Override
-	public String parse() {
-		throw new UnsupportedOperationException();
+	public String parse() throws DBEngineException {
+		String tableName = stmnt.getTargetTable().getTableName().toString();
+		List<TExpression> conditions = check();
+		Hashtable<String, String> htblColNameValue = new Hashtable<String, String>();
+		for (TExpression exp : conditions) {
+			htblColNameValue.put(exp.getLeftOperand().toString(), exp
+					.getRightOperand().toString());
+		}
+		Iterator<?> iter = dbEngine.selectFromTable(tableName,
+				htblColNameValue, operator);
+		Set<String> colName = Table.getInstance(tableName).getColsName();
+		String ColNamesFormated = "";
+		for (int i = 0; i < colName.size(); i++) {
+			ColNamesFormated += "%10s|";
+		}
+		ColNamesFormated += "\n";
+		String res = String.format(ColNamesFormated, colName.toArray());
+		Object[] values = new String[colName.size()];
+		while (iter.hasNext()) {
+			Record r = (Record) iter.next();
+			Hashtable<String, String> nameValue = r.getHtblColNameValue();
+			int i = 0;
+			for (String s : colName) {
+				values[i] = nameValue.get(s);
+				i++;
+			}
+			res += String.format(ColNamesFormated, values);
+		}
+		return res;
 	}
 }
