0fd2c47afe628e9c234de2982d027426796e09cc
Khaled Helmy <khaled.a.helmy@gmail.com>
2014-06-08 16:17:07 +0200

    Added proper locks for BufferManager

--- src/BufferManager/BufferManager.java
+++ src/BufferManager/BufferManager.java
@@ -3,6 +3,7 @@ package BufferManager;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 
@@ -13,22 +14,22 @@ import Utilities.Memory;
 import Utilities.Record;
 
 public class BufferManager implements BufferManagerInterface {
-	private static BufferManager bufferManager = null;  
+	private static BufferManager bufferManager = null;
 	private BufferUpdate bufferUpdate;
-	
+
 	private int size;
 	private FullPage[] memory;
-	private boolean[] modified;
+	private boolean[] modified, usedForRead, usedForWrite;
 	private LinkedList<Integer> LRU;
-	
+
 	private int minEmptySlotSize, maxUsedSlotSize;
 	private PriorityQueue<Integer> emptySlots, usedSlots;
-	
+
 	private BufferManager() {
 		bufferUpdate = new BufferUpdate();
 		bufferUpdate.start();
 	}
-	
+
 	public static BufferManager getBufferManager() {
 		if (bufferManager == null)
 			bufferManager = new BufferManager();
@@ -40,10 +41,12 @@ public class BufferManager implements BufferManagerInterface {
 		size = maxUsedSlotSize;
 		memory = new FullPage[size];
 		modified = new boolean[size];
+		usedForRead = new boolean[size];
+		usedForWrite = new boolean[size];
 		LRU = new LinkedList<Integer>();
 		emptySlots = new PriorityQueue<Integer>();
 		usedSlots = new PriorityQueue<Integer>();
-		for (int i=0; i<size; i++) {
+		for (int i = 0; i < size; i++) {
 			emptySlots.add(new Integer(i));
 		}
 	}
@@ -52,13 +55,23 @@ public class BufferManager implements BufferManagerInterface {
 		// Set locks!
 		int slot = getSlotNumber(pageID);
 		if (slot != -1) {
+			if (usedForWrite[slot] == true
+					|| (usedForRead[slot] == true && bModify == true)) {
+				return;
+			}
 			page.setRecords(memory[slot].getPage().getRecords());
-		}
-		else {
+		} else {
 			slot = emptySlots.poll().intValue();
 			usedSlots.add(new Integer(slot));
 			checkSizeConstraints();
-			// Load page
+			Hashtable<Integer, Record> records = Memory.getMemory().loadPage(
+					pageID.getPageNameTrimmed());
+			page.setRecords(records);
+			if (bModify == true) {
+				usedForWrite[slot] = true;
+			} else {
+				usedForRead[slot] = true;
+			}
 		}
 		modified[slot] = bModify;
 		Integer slotObj = new Integer(slot);
@@ -67,7 +80,8 @@ public class BufferManager implements BufferManagerInterface {
 	}
 
 	private void checkSizeConstraints() {
-		if (emptySlots.size() == minEmptySlotSize || usedSlots.size() == maxUsedSlotSize) {
+		if (emptySlots.size() == minEmptySlotSize
+				|| usedSlots.size() == maxUsedSlotSize) {
 			runLRU();
 		}
 		return;
@@ -80,9 +94,10 @@ public class BufferManager implements BufferManagerInterface {
 			LRU.remove(slotObj);
 			LRU.add(slotObj);
 			final String path = "data/" + pageID.getPageName();
-			ArrayList<Record> records = (ArrayList<Record>) page.getAllRecords();
+			ArrayList<Record> records = (ArrayList<Record>) page
+					.getAllRecords();
 			FileWriter fw = new FileWriter(path, true);
-			for (int i=0; i<records.size(); i++) {
+			for (int i = 0; i < records.size(); i++) {
 				Record rec = records.get(i);
 				fw.write(rec.toString() + "\n");
 			}
@@ -91,12 +106,11 @@ public class BufferManager implements BufferManagerInterface {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public synchronized void runLRU() {
 		if (LRU.size() == 0) {
 			return;
-		}
-		else {
+		} else {
 			int slot = LRU.getFirst().intValue();
 			LRU.removeFirst();
 			FullPage full = memory[slot];
@@ -106,29 +120,34 @@ public class BufferManager implements BufferManagerInterface {
 				write(id, page);
 			}
 			memory[slot] = null;
+			modified[slot] = false;
+			usedForRead[slot] = false;
+			usedForWrite[slot] = false;
 			Integer slotObj = new Integer(slot);
 			emptySlots.add(slotObj);
 			usedSlots.remove(slotObj);
 		}
 	}
-	
+
 	private void setSlotSizes() {
 		setMinEmptySlotSize();
 		setMaxUsedSlotSize();
 	}
 
 	private void setMinEmptySlotSize() {
-		String property = Memory.getMemory().getProperty("MinimumEmptyBufferSlots");
+		String property = Memory.getMemory().getProperty(
+				"MinimumEmptyBufferSlots");
 		minEmptySlotSize = Integer.parseInt(property);
 	}
-	
+
 	private void setMaxUsedSlotSize() {
-		String property = Memory.getMemory().getProperty("MaximumUsedBufferSlots");
+		String property = Memory.getMemory().getProperty(
+				"MaximumUsedBufferSlots");
 		maxUsedSlotSize = Integer.parseInt(property);
 	}
-	
+
 	private int getSlotNumber(PageID page) {
-		for (int i=0; i<size; i++) {
+		for (int i = 0; i < size; i++) {
 			if (memory[i].getId().equals(page)) {
 				return i;
 			}
